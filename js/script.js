a70f7906098e07783839e257b883d7f1fef59c4a
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-04-24 01:47:50 -0700

    Worked on RPS

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function rpsGame(yourChoice) {
     var humanChoice, botChoice;
     // humanChoice = yourChoice.id;
     botChoice = numberToChoice(randToRpsInt());
-    // console.log(botChoice);
+    console.log(botChoice);
     // results = decideWinner(humanChoice, botChoice); // [0, 1] human lost | bot won;
     // message = finalMessage(results); // {'message': 'You won!', 'color': 'green'}
     // rpsFrontEnd(yourChoice.id, botChoice, message);
@@ -36,8 +36,20 @@ function rpsGame(yourChoice) {
 
 function randToRpsInt() {
     return Math.floor(Math.random() * 3);
-}
+};
 
 function numberToChoice(number) {
-    return ['rock', 'paper', 'scissors'][number]
+    return ['rock', 'paper', 'scissors'][number];
+};
+
+function decideWinner(yourChoice, computerChoice) {
+    var rpsDatabase = {
+        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+        'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
+    }
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+    return [yourScore][computerScore];
 }
